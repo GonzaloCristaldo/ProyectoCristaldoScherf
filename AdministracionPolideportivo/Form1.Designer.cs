f0157c70d498c9960f301d7ab9572620fa63ee0c
Lucas <Lucas@DESKTOP-250LNCS>
2024-08-28 10:20:38 -0300

    Diseño de inicio de sesion

--- AdministracionPolideportivo/Form1.Designer.cs
+++ AdministracionPolideportivo/Form1.Designer.cs
@@ -28,12 +28,77 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            lblUsuario = new Label();
+            lblContrasena = new Label();
+            txtUsuario = new TextBox();
+            txtContrasena = new TextBox();
+            btnEntrar = new Button();
+            SuspendLayout();
+            // 
+            // lblUsuario
+            // 
+            lblUsuario.AutoSize = true;
+            lblUsuario.Location = new Point(134, 104);
+            lblUsuario.Name = "lblUsuario";
+            lblUsuario.Size = new Size(47, 15);
+            lblUsuario.TabIndex = 0;
+            lblUsuario.Text = "Usuario";
+            // 
+            // lblContrasena
+            // 
+            lblContrasena.AutoSize = true;
+            lblContrasena.Location = new Point(125, 156);
+            lblContrasena.Name = "lblContrasena";
+            lblContrasena.Size = new Size(67, 15);
+            lblContrasena.TabIndex = 1;
+            lblContrasena.Text = "Contraseña";
+            // 
+            // txtUsuario
+            // 
+            txtUsuario.Location = new Point(219, 101);
+            txtUsuario.Name = "txtUsuario";
+            txtUsuario.Size = new Size(189, 23);
+            txtUsuario.TabIndex = 2;
+            // 
+            // txtContrasena
+            // 
+            txtContrasena.Location = new Point(219, 156);
+            txtContrasena.Name = "txtContrasena";
+            txtContrasena.PasswordChar = '*';
+            txtContrasena.Size = new Size(189, 23);
+            txtContrasena.TabIndex = 3;
+            // 
+            // btnEntrar
+            // 
+            btnEntrar.Location = new Point(200, 235);
+            btnEntrar.Name = "btnEntrar";
+            btnEntrar.Size = new Size(114, 23);
+            btnEntrar.TabIndex = 4;
+            btnEntrar.Text = "Iniciar Sesión";
+            btnEntrar.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnEntrar);
+            Controls.Add(txtContrasena);
+            Controls.Add(txtUsuario);
+            Controls.Add(lblContrasena);
+            Controls.Add(lblUsuario);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label lblUsuario;
+        private Label lblContrasena;
+        private TextBox txtUsuario;
+        private TextBox txtContrasena;
+        private Button btnEntrar;
     }
 }
