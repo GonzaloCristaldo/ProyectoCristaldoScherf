f0b413e610faf26a45b66d7bb56591392e66a07e
Lucas <Lucas@DESKTOP-250LNCS>
2024-09-04 10:52:39 -0300

    Agregados botones y paneles personalizados

---
+++ AdministracionPolideportivo/CustomButton.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdministracionPolideportivo
+{
+    internal class CustomButton : Button
+    {
+        public CustomButton()
+        {
+            BackColor = Color.Green;
+        }
+    }
+}
---
+++ AdministracionPolideportivo/CustomPanel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdministracionPolideportivo
+{
+    internal class CustomPanel : Panel
+    {
+        public CustomPanel()
+        {
+            this.BackColor = Color.Red;
+        }
+    }
+}
