c30732422b200e3f77f0f6b56202b3af06df46a7
Gonzalo Cristaldo <gonzalodlp97@gmail.com>
2024-09-18 10:32:54 -0300

    Agregado botones vista de Admin

--- AdministracionPolideportivo/CPresentacion/Admin/vistaAdmin.cs
+++ AdministracionPolideportivo/CPresentacion/Admin/vistaAdmin.cs
@@ -16,33 +16,143 @@ namespace AdministracionPolideportivo.CPresentacion
         private void InitializeComponent()
         {
             label3 = new Label();
+            label1 = new Label();
+            label2 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            label4 = new Label();
+            dataGridView1 = new DataGridView();
+            panelNavegacion.SuspendLayout();
+            panelOpciones.SuspendLayout();
             panelFormulario.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
+            // 
+            // panelNavegacion
+            // 
+            panelNavegacion.Controls.Add(button2);
+            panelNavegacion.Controls.Add(button1);
+            panelNavegacion.Controls.Add(label1);
+            // 
+            // panelOpciones
+            // 
+            panelOpciones.Controls.Add(label4);
+            panelOpciones.Controls.Add(label2);
+            panelOpciones.Controls.Add(label3);
             // 
             // panelFormulario
             // 
-            panelFormulario.Controls.Add(label3);
+            panelFormulario.Controls.Add(dataGridView1);
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(109, 35);
+            label3.ForeColor = SystemColors.ButtonFace;
+            label3.Location = new Point(19, 60);
             label3.Name = "label3";
-            label3.Size = new Size(38, 15);
+            label3.Size = new Size(89, 15);
             label3.TabIndex = 0;
-            label3.Text = "label3";
+            label3.Text = "Gestionar Socio";
+            label3.Click += label3_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.ForeColor = SystemColors.ButtonHighlight;
+            label1.Location = new Point(286, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(131, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Gestion Socios del Club";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.ForeColor = SystemColors.ControlLightLight;
+            label2.Location = new Point(19, 101);
+            label2.Name = "label2";
+            label2.Size = new Size(117, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Ver Listado de Socios";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(619, 5);
+            button1.Name = "button1";
+            button1.Size = new Size(21, 23);
+            button1.TabIndex = 1;
+            button1.Text = "-";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(646, 5);
+            button2.Name = "button2";
+            button2.Size = new Size(24, 23);
+            button2.TabIndex = 2;
+            button2.Text = "X";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.ForeColor = SystemColors.ButtonFace;
+            label4.Location = new Point(19, 142);
+            label4.Name = "label4";
+            label4.Size = new Size(105, 15);
+            label4.TabIndex = 2;
+            label4.Text = "Gestionar Canchas";
+            label4.Click += label4_Click;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(61, 60);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(379, 238);
+            dataGridView1.TabIndex = 0;
             // 
             // VistaAdmin
             // 
             ClientSize = new Size(694, 450);
             Name = "VistaAdmin";
+            panelNavegacion.ResumeLayout(false);
+            panelNavegacion.PerformLayout();
+            panelOpciones.ResumeLayout(false);
+            panelOpciones.PerformLayout();
             panelFormulario.ResumeLayout(false);
-            panelFormulario.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
         }
 
         private Label label2;
         private Label label3;
+        private Button button1;
+        private Button button2;
+        private Label label4;
+        private DataGridView dataGridView1;
         private Label label1;
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
