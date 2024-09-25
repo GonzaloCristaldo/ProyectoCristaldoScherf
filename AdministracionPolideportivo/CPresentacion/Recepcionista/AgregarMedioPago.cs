1b70f4119604fd0b44a493833235d086a26944e0
Lucas <Lucas@DESKTOP-250LNCS>
2024-09-25 11:28:14 -0300

    Creados formulario de agregarmediopago y cobrar reserva

---
+++ AdministracionPolideportivo/CPresentacion/Recepcionista/AgregarMedioPago.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdministracionPolideportivo.CPresentacion.Recepcionista
+{
+    internal class AgregarMedioPago : FormularioEstandar
+    {
+
+        public AgregarMedioPago()
+        {
+
+            InitializeComponent();
+
+        }
+
+        
+
+        private void InitializeComponent()
+        {
+
+        }
+    }
+}
---
+++ AdministracionPolideportivo/CPresentacion/Recepcionista/CobrarReserva.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdministracionPolideportivo.CPresentacion.Recepcionista
+{
+    internal class CobrarReserva : FormularioEstandar
+    {
+
+        public CobrarReserva() {
+            InitializeComponent();
+        }
+
+        
+
+        private void InitializeComponent()
+        {
+
+        }
+    }
+}
