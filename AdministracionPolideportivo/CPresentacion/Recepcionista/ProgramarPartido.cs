edabc880f42a9ff27e008a2cba9d342869da9f80
Lucas <Lucas@DESKTOP-250LNCS>
2024-09-25 08:09:05 -0300

    Agregados reserva de servicios adicionales al formulario de programar partido

--- AdministracionPolideportivo/CPresentacion/Recepcionista/ProgramarPartido.cs
+++ AdministracionPolideportivo/CPresentacion/Recepcionista/ProgramarPartido.cs
@@ -30,6 +30,11 @@ namespace AdministracionPolideportivo.CPresentacion.Recepcionista
             comboBoxEstandar3 = new ComboBoxEstandar();
             labelFormulario5 = new LabelFormulario();
             btnAgendar = new BotonFormulario();
+            cbServicioAdicional = new ComboBoxEstandar();
+            lblServicio = new LabelFormulario();
+            btnAgregarServicio = new BotonFormulario();
+            txtServiciosAgregados = new Texto();
+            labelFormulario6 = new LabelFormulario();
             SuspendLayout();
             // 
             // comboBoxEstandar1
@@ -37,7 +42,7 @@ namespace AdministracionPolideportivo.CPresentacion.Recepcionista
             comboBoxEstandar1.BackColor = SystemColors.WindowFrame;
             comboBoxEstandar1.ForeColor = Color.White;
             comboBoxEstandar1.FormattingEnabled = true;
-            comboBoxEstandar1.Location = new Point(24, 92);
+            comboBoxEstandar1.Location = new Point(34, 65);
             comboBoxEstandar1.Name = "comboBoxEstandar1";
             comboBoxEstandar1.Size = new Size(274, 23);
             comboBoxEstandar1.TabIndex = 0;
@@ -46,7 +51,7 @@ namespace AdministracionPolideportivo.CPresentacion.Recepcionista
             // 
             labelFormulario1.AutoSize = true;
             labelFormulario1.ForeColor = Color.White;
-            labelFormulario1.Location = new Point(85, 19);
+            labelFormulario1.Location = new Point(256, 20);
             labelFormulario1.Name = "labelFormulario1";
             labelFormulario1.Size = new Size(142, 15);
             labelFormulario1.TabIndex = 1;
@@ -56,7 +61,7 @@ namespace AdministracionPolideportivo.CPresentacion.Recepcionista
             // 
             labelFormulario2.AutoSize = true;
             labelFormulario2.ForeColor = Color.White;
-            labelFormulario2.Location = new Point(118, 65);
+            labelFormulario2.Location = new Point(128, 38);
             labelFormulario2.Name = "labelFormulario2";
             labelFormulario2.Size = new Size(47, 15);
             labelFormulario2.TabIndex = 2;
@@ -66,7 +71,7 @@ namespace AdministracionPolideportivo.CPresentacion.Recepcionista
             // 
             labelFormulario3.AutoSize = true;
             labelFormulario3.ForeColor = Color.White;
-            labelFormulario3.Location = new Point(118, 125);
+            labelFormulario3.Location = new Point(128, 98);
             labelFormulario3.Name = "labelFormulario3";
             labelFormulario3.Size = new Size(50, 15);
             labelFormulario3.TabIndex = 4;
@@ -77,14 +82,14 @@ namespace AdministracionPolideportivo.CPresentacion.Recepcionista
             comboBoxEstandar2.BackColor = SystemColors.WindowFrame;
             comboBoxEstandar2.ForeColor = Color.White;
             comboBoxEstandar2.FormattingEnabled = true;
-            comboBoxEstandar2.Location = new Point(24, 152);
+            comboBoxEstandar2.Location = new Point(34, 125);
             comboBoxEstandar2.Name = "comboBoxEstandar2";
             comboBoxEstandar2.Size = new Size(274, 23);
             comboBoxEstandar2.TabIndex = 3;
             // 
             // dateTimePicker1
             // 
-            dateTimePicker1.Location = new Point(24, 210);
+            dateTimePicker1.Location = new Point(34, 183);
             dateTimePicker1.Name = "dateTimePicker1";
             dateTimePicker1.Size = new Size(274, 23);
             dateTimePicker1.TabIndex = 5;
@@ -93,7 +98,7 @@ namespace AdministracionPolideportivo.CPresentacion.Recepcionista
             // 
             labelFormulario4.AutoSize = true;
             labelFormulario4.ForeColor = Color.White;
-            labelFormulario4.Location = new Point(118, 192);
+            labelFormulario4.Location = new Point(128, 165);
             labelFormulario4.Name = "labelFormulario4";
             labelFormulario4.Size = new Size(41, 15);
             labelFormulario4.TabIndex = 6;
@@ -105,7 +110,7 @@ namespace AdministracionPolideportivo.CPresentacion.Recepcionista
             comboBoxEstandar3.ForeColor = Color.White;
             comboBoxEstandar3.FormattingEnabled = true;
             comboBoxEstandar3.Items.AddRange(new object[] { "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00" });
-            comboBoxEstandar3.Location = new Point(96, 277);
+            comboBoxEstandar3.Location = new Point(106, 250);
             comboBoxEstandar3.Name = "comboBoxEstandar3";
             comboBoxEstandar3.Size = new Size(82, 23);
             comboBoxEstandar3.TabIndex = 0;
@@ -114,7 +119,7 @@ namespace AdministracionPolideportivo.CPresentacion.Recepcionista
             // 
             labelFormulario5.AutoSize = true;
             labelFormulario5.ForeColor = Color.White;
-            labelFormulario5.Location = new Point(118, 259);
+            labelFormulario5.Location = new Point(128, 232);
             labelFormulario5.Name = "labelFormulario5";
             labelFormulario5.Size = new Size(36, 15);
             labelFormulario5.TabIndex = 7;
@@ -125,17 +130,75 @@ namespace AdministracionPolideportivo.CPresentacion.Recepcionista
             btnAgendar.BackColor = Color.DimGray;
             btnAgendar.Font = new Font("Segoe UI", 10F);
             btnAgendar.ForeColor = Color.White;
-            btnAgendar.Location = new Point(85, 334);
+            btnAgendar.Location = new Point(82, 325);
             btnAgendar.Name = "btnAgendar";
             btnAgendar.Size = new Size(146, 41);
             btnAgendar.TabIndex = 0;
             btnAgendar.Text = "Agendar Partido";
             btnAgendar.UseVisualStyleBackColor = false;
             // 
+            // cbServicioAdicional
+            // 
+            cbServicioAdicional.BackColor = SystemColors.WindowFrame;
+            cbServicioAdicional.ForeColor = Color.White;
+            cbServicioAdicional.FormattingEnabled = true;
+            cbServicioAdicional.Location = new Point(394, 65);
+            cbServicioAdicional.Name = "cbServicioAdicional";
+            cbServicioAdicional.Size = new Size(210, 23);
+            cbServicioAdicional.TabIndex = 0;
+            // 
+            // lblServicio
+            // 
+            lblServicio.AutoSize = true;
+            lblServicio.ForeColor = Color.White;
+            lblServicio.Location = new Point(452, 38);
+            lblServicio.Name = "lblServicio";
+            lblServicio.Size = new Size(101, 15);
+            lblServicio.TabIndex = 8;
+            lblServicio.Text = "Servicio Adicional";
+            // 
+            // btnAgregarServicio
+            // 
+            btnAgregarServicio.BackColor = Color.DimGray;
+            btnAgregarServicio.Font = new Font("Segoe UI", 10F);
+            btnAgregarServicio.ForeColor = Color.White;
+            btnAgregarServicio.Location = new Point(432, 107);
+            btnAgregarServicio.Name = "btnAgregarServicio";
+            btnAgregarServicio.Size = new Size(146, 41);
+            btnAgregarServicio.TabIndex = 0;
+            btnAgregarServicio.Text = "Agregar Servicio";
+            btnAgregarServicio.UseVisualStyleBackColor = false;
+            // 
+            // txtServiciosAgregados
+            // 
+            txtServiciosAgregados.BackColor = SystemColors.WindowFrame;
+            txtServiciosAgregados.Enabled = false;
+            txtServiciosAgregados.ForeColor = Color.White;
+            txtServiciosAgregados.Location = new Point(359, 186);
+            txtServiciosAgregados.Multiline = true;
+            txtServiciosAgregados.Name = "txtServiciosAgregados";
+            txtServiciosAgregados.Size = new Size(291, 180);
+            txtServiciosAgregados.TabIndex = 0;
+            // 
+            // labelFormulario6
+            // 
+            labelFormulario6.AutoSize = true;
+            labelFormulario6.ForeColor = Color.White;
+            labelFormulario6.Location = new Point(125, 296);
+            labelFormulario6.Name = "labelFormulario6";
+            labelFormulario6.Size = new Size(50, 15);
+            labelFormulario6.TabIndex = 9;
+            labelFormulario6.Text = "Total: $0";
+            // 
             // ProgramarPartido
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
-            ClientSize = new Size(323, 396);
+            ClientSize = new Size(662, 396);
+            Controls.Add(labelFormulario6);
+            Controls.Add(txtServiciosAgregados);
+            Controls.Add(btnAgregarServicio);
+            Controls.Add(lblServicio);
+            Controls.Add(cbServicioAdicional);
             Controls.Add(btnAgendar);
             Controls.Add(labelFormulario5);
             Controls.Add(comboBoxEstandar3);
@@ -160,6 +223,11 @@ namespace AdministracionPolideportivo.CPresentacion.Recepcionista
         private ComboBoxEstandar comboBoxEstandar3;
         private LabelFormulario labelFormulario5;
         private BotonFormulario btnAgendar;
+        private ComboBoxEstandar cbServicioAdicional;
+        private LabelFormulario lblServicio;
+        private BotonFormulario btnAgregarServicio;
+        private Texto txtServiciosAgregados;
+        private LabelFormulario labelFormulario6;
         private ComboBoxEstandar comboBoxEstandar2;
     }
 }
