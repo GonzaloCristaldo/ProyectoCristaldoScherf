39ede3aa8f8dfea9c7cba672fb926c7def845661
Gonzalo Cristaldo <gonzalodlp97@gmail.com>
2024-09-29 04:49:04 -0300

    Creado agregarUsuario e implementacion del mismo en SuperAdmin

--- AdministracionPolideportivo/CPresentacion/SuperAdmin/vistaSuperAdmin.cs
+++ AdministracionPolideportivo/CPresentacion/SuperAdmin/vistaSuperAdmin.cs
@@ -5,8 +5,144 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace AdministracionPolideportivo.CPresentacion.SuperAdmin
+
 {
     internal class vistaSuperAdmin : VentanaMenu
     {
+        public vistaSuperAdmin()
+        {
+
+            InitializeComponent();
+            contadorOpciones = 0;
+            //tablaOpciones.Width = panelOpciones.Width;
+
+            btnMinimizar.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            btnCerrar.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+
+            AgregarBotonOpcion(new BotonOpcion(new agregarUsuario(), this, formActual, "Agregar Usuario de sistema"));
+        }
+        private void InitializeComponent()
+        {
+            tablaOpciones = new TableLayoutPanel();
+            btnCerrar = new Button();
+            btnMinimizar = new Button();
+            panelNavegacion.SuspendLayout();
+            panelOpciones.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panelNavegacion
+            // 
+            panelNavegacion.Controls.Add(btnMinimizar);
+            panelNavegacion.Controls.Add(btnCerrar);
+            panelNavegacion.Size = new Size(1090, 34);
+            // 
+            // panelOpciones
+            // 
+            panelOpciones.AutoScroll = true;
+            panelOpciones.Controls.Add(tablaOpciones);
+            panelOpciones.Size = new Size(226, 406);
+            // 
+            // panelFormulario
+            // 
+            panelFormulario.Size = new Size(864, 406);
+            // 
+            // tablaOpciones
+            // 
+            tablaOpciones.AutoSize = true;
+            tablaOpciones.ColumnCount = 1;
+            tablaOpciones.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tablaOpciones.Dock = DockStyle.Top;
+            tablaOpciones.Location = new Point(0, 0);
+            tablaOpciones.Name = "tablaOpciones";
+            tablaOpciones.RowCount = 1;
+            tablaOpciones.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tablaOpciones.Size = new Size(226, 0);
+            tablaOpciones.TabIndex = 0;
+            // 
+            // btnCerrar
+            // 
+            btnCerrar.Location = new Point(1060, 3);
+            btnCerrar.Name = "btnCerrar";
+            btnCerrar.Size = new Size(27, 24);
+            btnCerrar.TabIndex = 6;
+            btnCerrar.Text = "X";
+            btnCerrar.UseVisualStyleBackColor = true;
+            btnCerrar.Click += btnCerrar_Click;
+            // 
+            // btnMinimizar
+            // 
+            btnMinimizar.Location = new Point(1029, 3);
+            btnMinimizar.Name = "btnMinimizar";
+            btnMinimizar.Size = new Size(25, 24);
+            btnMinimizar.TabIndex = 7;
+            btnMinimizar.Text = "-";
+            btnMinimizar.UseVisualStyleBackColor = true;
+            btnMinimizar.Click += btnMinimizar_Click;
+            // 
+            // VistaAdmin
+            // 
+            ClientSize = new Size(1100, 450);
+            Name = "VistaAdmin";
+            panelNavegacion.ResumeLayout(false);
+            panelOpciones.ResumeLayout(false);
+            panelOpciones.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        private Button button1;
+        private Button button2;
+        private Button btnCerrar;
+        private Button btnMinimizar;
+        private Label label1;
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void botonFormulario1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private TableLayoutPanel tablaOpciones;
+        private int contadorOpciones;
+
+
+        private void AgregarBotonOpcion(BotonOpcion nuevoBoton)
+        {
+            /*Se agrega el boton a la tabla de opciones, en la primer columna
+            (0) y en la fila correspondiente al contador*/
+            tablaOpciones.Controls.Add(nuevoBoton, 0, contadorOpciones);
+            //Se actualiza el contador
+            contadorOpciones++;
+            //Se agrega la opcion al array definido en la superclase
+            agregarOpcion(nuevoBoton);
+        }
+
+        private void btnMinimizar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
     }
 }
---
+++ AdministracionPolideportivo/CPresentacion/agregarUsuario.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdministracionPolideportivo.CPresentacion
+{
+    internal class agregarUsuario : FormularioEstandar
+    {
+        public agregarUsuario()
+        {
+
+            InitializeComponent();
+
+        }
+
+        private LabelFormulario lblDNI;
+        private LabelFormulario lblNombre;
+        private Texto txtNombre;
+        private LabelFormulario lblApellido;
+        private Texto txtApellido;
+        private LabelFormulario lblTelefono;
+        private TextoNumerico textoNumerico1;
+        private BotonFormulario btnAgregarCliente;
+        private ComboBoxEstandar comboBoxEstandar1;
+        private LabelFormulario labelFormulario1;
+        private TextoNumerico txtDNI;
+
+        private void InitializeComponent()
+        {
+            lblDNI = new LabelFormulario();
+            txtDNI = new TextoNumerico();
+            lblNombre = new LabelFormulario();
+            txtNombre = new Texto();
+            lblApellido = new LabelFormulario();
+            txtApellido = new Texto();
+            lblTelefono = new LabelFormulario();
+            textoNumerico1 = new TextoNumerico();
+            btnAgregarCliente = new BotonFormulario();
+            comboBoxEstandar1 = new ComboBoxEstandar();
+            labelFormulario1 = new LabelFormulario();
+            SuspendLayout();
+            // 
+            // lblDNI
+            // 
+            lblDNI.AutoSize = true;
+            lblDNI.ForeColor = Color.White;
+            lblDNI.Location = new Point(22, 21);
+            lblDNI.Name = "lblDNI";
+            lblDNI.Size = new Size(36, 15);
+            lblDNI.TabIndex = 0;
+            lblDNI.Text = "D.N.I.";
+            // 
+            // txtDNI
+            // 
+            txtDNI.BackColor = SystemColors.WindowFrame;
+            txtDNI.ForeColor = Color.White;
+            txtDNI.Location = new Point(87, 18);
+            txtDNI.Name = "txtDNI";
+            txtDNI.Size = new Size(117, 23);
+            txtDNI.TabIndex = 1;
+            // 
+            // lblNombre
+            // 
+            lblNombre.AutoSize = true;
+            lblNombre.ForeColor = Color.White;
+            lblNombre.Location = new Point(22, 67);
+            lblNombre.Name = "lblNombre";
+            lblNombre.Size = new Size(51, 15);
+            lblNombre.TabIndex = 2;
+            lblNombre.Text = "Nombre";
+            // 
+            // txtNombre
+            // 
+            txtNombre.BackColor = SystemColors.WindowFrame;
+            txtNombre.ForeColor = Color.White;
+            txtNombre.Location = new Point(87, 64);
+            txtNombre.Name = "txtNombre";
+            txtNombre.Size = new Size(210, 23);
+            txtNombre.TabIndex = 0;
+            // 
+            // lblApellido
+            // 
+            lblApellido.AutoSize = true;
+            lblApellido.ForeColor = Color.White;
+            lblApellido.Location = new Point(22, 118);
+            lblApellido.Name = "lblApellido";
+            lblApellido.Size = new Size(51, 15);
+            lblApellido.TabIndex = 3;
+            lblApellido.Text = "Apellido";
+            // 
+            // txtApellido
+            // 
+            txtApellido.BackColor = SystemColors.WindowFrame;
+            txtApellido.ForeColor = Color.White;
+            txtApellido.Location = new Point(87, 115);
+            txtApellido.Name = "txtApellido";
+            txtApellido.Size = new Size(210, 23);
+            txtApellido.TabIndex = 0;
+            // 
+            // lblTelefono
+            // 
+            lblTelefono.AutoSize = true;
+            lblTelefono.ForeColor = Color.White;
+            lblTelefono.Location = new Point(21, 206);
+            lblTelefono.Name = "lblTelefono";
+            lblTelefono.Size = new Size(52, 15);
+            lblTelefono.TabIndex = 4;
+            lblTelefono.Text = "Telefono";
+            // 
+            // textoNumerico1
+            // 
+            textoNumerico1.BackColor = SystemColors.WindowFrame;
+            textoNumerico1.ForeColor = Color.White;
+            textoNumerico1.Location = new Point(87, 203);
+            textoNumerico1.Name = "textoNumerico1";
+            textoNumerico1.Size = new Size(151, 23);
+            textoNumerico1.TabIndex = 1;
+            // 
+            // btnAgregarCliente
+            // 
+            btnAgregarCliente.BackColor = Color.LimeGreen;
+            btnAgregarCliente.Font = new Font("Segoe UI", 10F);
+            btnAgregarCliente.ForeColor = Color.White;
+            btnAgregarCliente.Location = new Point(92, 247);
+            btnAgregarCliente.Name = "btnAgregarCliente";
+            btnAgregarCliente.Size = new Size(146, 41);
+            btnAgregarCliente.TabIndex = 0;
+            btnAgregarCliente.Text = "Agregar Usuario";
+            btnAgregarCliente.UseVisualStyleBackColor = false;
+            // 
+            // comboBoxEstandar1
+            // 
+            comboBoxEstandar1.BackColor = SystemColors.WindowFrame;
+            comboBoxEstandar1.ForeColor = Color.White;
+            comboBoxEstandar1.FormattingEnabled = true;
+            comboBoxEstandar1.Location = new Point(87, 160);
+            comboBoxEstandar1.Name = "comboBoxEstandar1";
+            comboBoxEstandar1.Size = new Size(210, 23);
+            comboBoxEstandar1.TabIndex = 0;
+            // 
+            // labelFormulario1
+            // 
+            labelFormulario1.AutoSize = true;
+            labelFormulario1.ForeColor = Color.White;
+            labelFormulario1.Location = new Point(21, 153);
+            labelFormulario1.Name = "labelFormulario1";
+            labelFormulario1.Size = new Size(47, 30);
+            labelFormulario1.TabIndex = 5;
+            labelFormulario1.Text = "Tipo de\r\nUsuario\r\n";
+            labelFormulario1.Click += labelFormulario1_Click;
+            // 
+            // agregarUsuario
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            ClientSize = new Size(336, 300);
+            Controls.Add(labelFormulario1);
+            Controls.Add(comboBoxEstandar1);
+            Controls.Add(btnAgregarCliente);
+            Controls.Add(textoNumerico1);
+            Controls.Add(lblTelefono);
+            Controls.Add(txtApellido);
+            Controls.Add(lblApellido);
+            Controls.Add(txtNombre);
+            Controls.Add(lblNombre);
+            Controls.Add(txtDNI);
+            Controls.Add(lblDNI);
+            Name = "agregarUsuario";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelFormulario1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
