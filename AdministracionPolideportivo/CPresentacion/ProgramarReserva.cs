68a4ec2559f59842c226e1fbbc747ba3621ffba8
Lucas <Lucas@DESKTOP-250LNCS>
2024-10-11 17:37:51 -0300

    agregado diseño responsivo a formulario de programarReserva

--- AdministracionPolideportivo/CPresentacion/ProgramarReserva.cs
+++ AdministracionPolideportivo/CPresentacion/ProgramarReserva.cs
@@ -14,29 +14,79 @@ namespace AdministracionPolideportivo.CPresentacion.Recepcionista
         {
 
             InitializeComponent();
-
+            ubiPanel1 = 20;
+            anchoPaneles = (this.Width / 2) - 50;
+            ubiPanel2 = anchoPaneles + 30;
+            UbicarControles();
         }
 
+        int anchoPaneles;
 
+        int ubiPanel1;
+        int ubiPanel2;
+
+
+        private void UbicarControles()
+        {
+            anchoPaneles = (this.Width / 2) - 25;
+            panelIzquierda.Width = anchoPaneles;
+            panelDerecha.Width = anchoPaneles;
+
+            ubiPanel1 = 20;
+
+            ubiPanel2 = anchoPaneles + 30;
+
+            panelIzquierda.Location = new Point(ubiPanel1, panelIzquierda.Location.Y);
+            panelDerecha.Location = new Point(ubiPanel2, panelDerecha.Location.Y);
+
+            cbCliente.Width = panelIzquierda.Width - 40;
+            cbCliente.Location = new Point(ubiPanel1, cbCliente.Location.Y);
+            cbRecinto.Width = panelIzquierda.Width - 40;
+            cbRecinto.Location = new Point(ubiPanel1, cbRecinto.Location.Y);
+            fecha.Width = panelIzquierda.Width - 40;
+            fecha.Location = new Point(ubiPanel1, fecha.Location.Y);
+
+            cbHora.Location = new Point((panelIzquierda.Width - cbHora.Width) / 2, cbHora.Location.Y);
+
+            cbServicioAdicional.Width = panelDerecha.Width - 40;
+            cbServicioAdicional.Location = new Point(ubiPanel1, cbServicioAdicional.Location.Y);
+
+            tabla.Width = panelDerecha.Width;
+            tabla.Location= new Point(0, btnAgregarServicio.Location.Y+btnAgregarServicio.Height+15);
+            btnAgregarServicio.Location = new Point((panelDerecha.Width - btnAgregarServicio.Width) / 2, btnAgregarServicio.Location.Y);
+            btnAgendar.Location = new Point((panelIzquierda.Width - btnAgendar.Width) / 2, btnAgendar.Location.Y);
+            lblServicio.Location = new Point((panelDerecha.Width-lblServicio.Width)/2,lblServicio.Location.Y);
+
+            lblCliente.Location = new Point((panelIzquierda.Width - lblCliente.Width) / 2, lblCliente.Location.Y);
+            lblRecinto.Location = new Point((panelIzquierda.Width - lblRecinto.Width) / 2, lblRecinto.Location.Y);
+            lblFecha.Location = new Point((panelIzquierda.Width - lblFecha.Width) / 2, lblFecha.Location.Y);
+            lblHora.Location = new Point((panelIzquierda.Width - lblHora.Width) / 2, lblHora.Location.Y);
+            lblTotal.Location = new Point((panelIzquierda.Width - lblTotal.Width) / 2, lblTotal.Location.Y);
+            lblTitulo.Location = new Point((this.Width - lblTitulo.Width) / 2, lblTitulo.Location.Y);
+        }
 
         private void InitializeComponent()
         {
             cbCliente = new ComboBoxEstandar();
-            labelFormulario1 = new LabelFormulario();
-            labelFormulario2 = new LabelFormulario();
-            labelFormulario3 = new LabelFormulario();
+            lblTitulo = new LabelFormulario();
+            lblCliente = new LabelFormulario();
+            lblRecinto = new LabelFormulario();
             cbRecinto = new ComboBoxEstandar();
             fecha = new DateTimePicker();
-            labelFormulario4 = new LabelFormulario();
+            lblFecha = new LabelFormulario();
             cbHora = new ComboBoxEstandar();
-            labelFormulario5 = new LabelFormulario();
+            lblHora = new LabelFormulario();
             btnAgendar = new BotonFormulario();
             cbServicioAdicional = new ComboBoxEstandar();
             lblServicio = new LabelFormulario();
             btnAgregarServicio = new BotonFormulario();
-            labelFormulario6 = new LabelFormulario();
-            dataGridView1 = new DataGridView();
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            lblTotal = new LabelFormulario();
+            tabla = new DataGridView();
+            panelDerecha = new Panel();
+            panelIzquierda = new Panel();
+            ((System.ComponentModel.ISupportInitialize)tabla).BeginInit();
+            panelDerecha.SuspendLayout();
+            panelIzquierda.SuspendLayout();
             SuspendLayout();
             // 
             // cbCliente
@@ -44,72 +94,72 @@ namespace AdministracionPolideportivo.CPresentacion.Recepcionista
             cbCliente.BackColor = SystemColors.WindowFrame;
             cbCliente.ForeColor = Color.White;
             cbCliente.FormattingEnabled = true;
-            cbCliente.Location = new Point(34, 69);
+            cbCliente.Location = new Point(23, 30);
             cbCliente.Name = "cbCliente";
             cbCliente.Size = new Size(274, 23);
             cbCliente.TabIndex = 0;
             // 
-            // labelFormulario1
+            // lblTitulo
             // 
-            labelFormulario1.AutoSize = true;
-            labelFormulario1.Font = new Font("Segoe UI", 15F);
-            labelFormulario1.ForeColor = Color.White;
-            labelFormulario1.Location = new Point(185, 9);
-            labelFormulario1.Name = "labelFormulario1";
-            labelFormulario1.Size = new Size(237, 28);
-            labelFormulario1.TabIndex = 1;
-            labelFormulario1.Text = "Programar Nueva Reserva";
-            labelFormulario1.Click += labelFormulario1_Click;
+            lblTitulo.AutoSize = true;
+            lblTitulo.Font = new Font("Segoe UI", 15F);
+            lblTitulo.ForeColor = Color.White;
+            lblTitulo.Location = new Point(185, 9);
+            lblTitulo.Name = "lblTitulo";
+            lblTitulo.Size = new Size(237, 28);
+            lblTitulo.TabIndex = 1;
+            lblTitulo.Text = "Programar Nueva Reserva";
+            lblTitulo.Click += labelFormulario1_Click;
             // 
-            // labelFormulario2
+            // lblCliente
             // 
-            labelFormulario2.AutoSize = true;
-            labelFormulario2.Font = new Font("Segoe UI", 15F);
-            labelFormulario2.ForeColor = Color.White;
-            labelFormulario2.Location = new Point(128, 38);
-            labelFormulario2.Name = "labelFormulario2";
-            labelFormulario2.Size = new Size(76, 28);
-            labelFormulario2.TabIndex = 2;
-            labelFormulario2.Text = "Cliente:";
+            lblCliente.AutoSize = true;
+            lblCliente.Font = new Font("Segoe UI", 15F);
+            lblCliente.ForeColor = Color.White;
+            lblCliente.Location = new Point(117, -1);
+            lblCliente.Name = "lblCliente";
+            lblCliente.Size = new Size(76, 28);
+            lblCliente.TabIndex = 2;
+            lblCliente.Text = "Cliente:";
             // 
-            // labelFormulario3
+            // lblRecinto
             // 
-            labelFormulario3.AutoSize = true;
-            labelFormulario3.Font = new Font("Segoe UI", 15F);
-            labelFormulario3.ForeColor = Color.White;
-            labelFormulario3.Location = new Point(128, 98);
-            labelFormulario3.Name = "labelFormulario3";
-            labelFormulario3.Size = new Size(81, 28);
-            labelFormulario3.TabIndex = 4;
-            labelFormulario3.Text = "Recinto:";
+            lblRecinto.AutoSize = true;
+            lblRecinto.Font = new Font("Segoe UI", 15F);
+            lblRecinto.ForeColor = Color.White;
+            lblRecinto.Location = new Point(117, 59);
+            lblRecinto.Name = "lblRecinto";
+            lblRecinto.Size = new Size(81, 28);
+            lblRecinto.TabIndex = 4;
+            lblRecinto.Text = "Recinto:";
             // 
             // cbRecinto
             // 
             cbRecinto.BackColor = SystemColors.WindowFrame;
             cbRecinto.ForeColor = Color.White;
             cbRecinto.FormattingEnabled = true;
-            cbRecinto.Location = new Point(34, 129);
+            cbRecinto.Location = new Point(23, 90);
             cbRecinto.Name = "cbRecinto";
             cbRecinto.Size = new Size(274, 23);
             cbRecinto.TabIndex = 3;
             // 
             // fecha
             // 
-            fecha.Location = new Point(34, 196);
+            fecha.Location = new Point(23, 157);
             fecha.Name = "fecha";
             fecha.Size = new Size(274, 23);
             fecha.TabIndex = 5;
             // 
-            // labelFormulario4
+            // lblFecha
             // 
-            labelFormulario4.AutoSize = true;
-            labelFormulario4.Font = new Font("Segoe UI", 15F);
-            labelFormulario4.ForeColor = Color.White;
-            labelFormulario4.Location = new Point(138, 165);
-            labelFormulario4.Name = "labelFormulario4";
-            labelFormulario4.Size = new Size(66, 28);
-            labelFormulario4.TabIndex = 6;
-            labelFormulario4.Text = "Fecha:";
+            lblFecha.AutoSize = true;
+            lblFecha.Font = new Font("Segoe UI", 15F);
+            lblFecha.ForeColor = Color.White;
+            lblFecha.Location = new Point(117, 126);
+            lblFecha.Name = "lblFecha";
+            lblFecha.Size = new Size(66, 28);
+            lblFecha.TabIndex = 6;
+            lblFecha.Text = "Fecha:";
             // 
             // cbHora
             // 
@@ -117,28 +167,28 @@ namespace AdministracionPolideportivo.CPresentacion.Recepcionista
             cbHora.ForeColor = Color.White;
             cbHora.FormattingEnabled = true;
             cbHora.Items.AddRange(new object[] { "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00" });
-            cbHora.Location = new Point(112, 263);
+            cbHora.Location = new Point(101, 224);
             cbHora.Name = "cbHora";
             cbHora.Size = new Size(82, 23);
             cbHora.TabIndex = 0;
             // 
-            // labelFormulario5
+            // lblHora
             // 
-            labelFormulario5.AutoSize = true;
-            labelFormulario5.Font = new Font("Segoe UI", 15F);
-            labelFormulario5.ForeColor = Color.White;
-            labelFormulario5.Location = new Point(128, 232);
-            labelFormulario5.Name = "labelFormulario5";
-            labelFormulario5.Size = new Size(59, 28);
-            labelFormulario5.TabIndex = 7;
-            labelFormulario5.Text = "Hora:";
+            lblHora.AutoSize = true;
+            lblHora.Font = new Font("Segoe UI", 15F);
+            lblHora.ForeColor = Color.White;
+            lblHora.Location = new Point(117, 193);
+            lblHora.Name = "lblHora";
+            lblHora.Size = new Size(59, 28);
+            lblHora.TabIndex = 7;
+            lblHora.Text = "Hora:";
             // 
             // btnAgendar
             // 
             btnAgendar.BackColor = Color.DimGray;
             btnAgendar.Font = new Font("Segoe UI", 10F);
             btnAgendar.ForeColor = Color.White;
-            btnAgendar.Location = new Point(92, 327);
+            btnAgendar.Location = new Point(81, 288);
             btnAgendar.Name = "btnAgendar";
             btnAgendar.Size = new Size(146, 41);
             btnAgendar.TabIndex = 0;
@@ -151,7 +201,7 @@ namespace AdministracionPolideportivo.CPresentacion.Recepcionista
             cbServicioAdicional.BackColor = SystemColors.WindowFrame;
             cbServicioAdicional.ForeColor = Color.White;
             cbServicioAdicional.FormattingEnabled = true;
-            cbServicioAdicional.Location = new Point(378, 79);
+            cbServicioAdicional.Location = new Point(52, 44);
             cbServicioAdicional.Name = "cbServicioAdicional";
             cbServicioAdicional.Size = new Size(210, 23);
             cbServicioAdicional.TabIndex = 0;
@@ -161,7 +211,7 @@ namespace AdministracionPolideportivo.CPresentacion.Recepcionista
             lblServicio.AutoSize = true;
             lblServicio.Font = new Font("Segoe UI", 15F);
             lblServicio.ForeColor = Color.White;
-            lblServicio.Location = new Point(399, 48);
+            lblServicio.Location = new Point(73, 13);
             lblServicio.Name = "lblServicio";
             lblServicio.Size = new Size(168, 28);
             lblServicio.TabIndex = 8;
@@ -172,71 +222,95 @@ namespace AdministracionPolideportivo.CPresentacion.Recepcionista
             btnAgregarServicio.BackColor = Color.DimGray;
             btnAgregarServicio.Font = new Font("Segoe UI", 10F);
             btnAgregarServicio.ForeColor = Color.White;
-            btnAgregarServicio.Location = new Point(412, 125);
+            btnAgregarServicio.Location = new Point(86, 90);
             btnAgregarServicio.Name = "btnAgregarServicio";
             btnAgregarServicio.Size = new Size(146, 41);
             btnAgregarServicio.TabIndex = 0;
             btnAgregarServicio.Text = "Agregar Servicio";
             btnAgregarServicio.UseVisualStyleBackColor = false;
             // 
-            // labelFormulario6
+            // lblTotal
             // 
-            labelFormulario6.AutoSize = true;
-            labelFormulario6.Font = new Font("Segoe UI", 15F);
-            labelFormulario6.ForeColor = Color.White;
-            labelFormulario6.Location = new Point(125, 296);
-            labelFormulario6.Name = "labelFormulario6";
-            labelFormulario6.Size = new Size(85, 28);
-            labelFormulario6.TabIndex = 9;
-            labelFormulario6.Text = "Total: $0";
+            lblTotal.AutoSize = true;
+            lblTotal.Font = new Font("Segoe UI", 15F);
+            lblTotal.ForeColor = Color.White;
+            lblTotal.Location = new Point(117, 257);
+            lblTotal.Name = "lblTotal";
+            lblTotal.Size = new Size(85, 28);
+            lblTotal.TabIndex = 9;
+            lblTotal.Text = "Total: $0";
             // 
-            // dataGridView1
+            // tabla
             // 
-            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(343, 183);
-            dataGridView1.Name = "dataGridView1";
-            dataGridView1.Size = new Size(292, 150);
-            dataGridView1.TabIndex = 10;
+            tabla.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            tabla.Location = new Point(17, 148);
+            tabla.Name = "tabla";
+            tabla.Size = new Size(292, 150);
+            tabla.TabIndex = 10;
+            // 
+            // panelDerecha
+            // 
+            panelDerecha.Controls.Add(cbServicioAdicional);
+            panelDerecha.Controls.Add(tabla);
+            panelDerecha.Controls.Add(lblServicio);
+            panelDerecha.Controls.Add(btnAgregarServicio);
+            panelDerecha.Location = new Point(347, 50);
+            panelDerecha.Name = "panelDerecha";
+            panelDerecha.Size = new Size(326, 347);
+            panelDerecha.TabIndex = 11;
+            // 
+            // panelIzquierda
+            // 
+            panelIzquierda.Controls.Add(cbCliente);
+            panelIzquierda.Controls.Add(lblCliente);
+            panelIzquierda.Controls.Add(lblTotal);
+            panelIzquierda.Controls.Add(cbRecinto);
+            panelIzquierda.Controls.Add(btnAgendar);
+            panelIzquierda.Controls.Add(lblRecinto);
+            panelIzquierda.Controls.Add(lblHora);
+            panelIzquierda.Controls.Add(fecha);
+            panelIzquierda.Controls.Add(cbHora);
+            panelIzquierda.Controls.Add(lblFecha);
+            panelIzquierda.Location = new Point(12, 50);
+            panelIzquierda.Name = "panelIzquierda";
+            panelIzquierda.Size = new Size(320, 347);
+            panelIzquierda.TabIndex = 12;
             // 
             // ProgramarReserva
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
-            ClientSize = new Size(662, 396);
-            Controls.Add(dataGridView1);
-            Controls.Add(labelFormulario6);
-            Controls.Add(btnAgregarServicio);
-            Controls.Add(lblServicio);
-            Controls.Add(cbServicioAdicional);
-            Controls.Add(btnAgendar);
-            Controls.Add(labelFormulario5);
-            Controls.Add(cbHora);
-            Controls.Add(labelFormulario4);
-            Controls.Add(fecha);
-            Controls.Add(labelFormulario3);
-            Controls.Add(cbRecinto);
-            Controls.Add(labelFormulario2);
-            Controls.Add(labelFormulario1);
-            Controls.Add(cbCliente);
+            ClientSize = new Size(688, 415);
+            Controls.Add(panelIzquierda);
+            Controls.Add(panelDerecha);
+            Controls.Add(lblTitulo);
+            MinimumSize = new Size(688, 415);
             Name = "ProgramarReserva";
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            Resize += ProgramarReserva_Resize;
+            ((System.ComponentModel.ISupportInitialize)tabla).EndInit();
+            panelDerecha.ResumeLayout(false);
+            panelDerecha.PerformLayout();
+            panelIzquierda.ResumeLayout(false);
+            panelIzquierda.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
 
         private ComboBoxEstandar cbCliente;
-        private LabelFormulario labelFormulario1;
-        private LabelFormulario labelFormulario2;
-        private LabelFormulario labelFormulario3;
+        private LabelFormulario lblTitulo;
+        private LabelFormulario lblCliente;
+        private LabelFormulario lblRecinto;
         private DateTimePicker fecha;
-        private LabelFormulario labelFormulario4;
+        private LabelFormulario lblFecha;
         private ComboBoxEstandar cbHora;
-        private LabelFormulario labelFormulario5;
+        private LabelFormulario lblHora;
         private BotonFormulario btnAgendar;
         private ComboBoxEstandar cbServicioAdicional;
         private LabelFormulario lblServicio;
         private BotonFormulario btnAgregarServicio;
-        private LabelFormulario labelFormulario6;
-        private DataGridView dataGridView1;
+        private LabelFormulario lblTotal;
+        private DataGridView tabla;
+        private Panel panelDerecha;
+        private Panel panelIzquierda;
         private ComboBoxEstandar cbRecinto;
 
         private void labelFormulario1_Click(object sender, EventArgs e)
@@ -282,6 +356,11 @@ namespace AdministracionPolideportivo.CPresentacion.Recepcionista
                 MessageBox.Show(mensaje);
                 return;
             }
+        }
+
+        private void ProgramarReserva_Resize(object sender, EventArgs e)
+        {
+           UbicarControles();
         }
     }
 }
